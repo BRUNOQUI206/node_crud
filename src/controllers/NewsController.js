3d370e3e29673c5701cb7eb32c7598ed99d4cedd
Bruno <brunoguilher1@gmail.com>
2024-05-15 17:32:16 -0300

    update post image

--- src/controllers/NewsController.js
+++ src/controllers/NewsController.js
@@ -1,69 +1,72 @@
-const NewsService = require('../services/NewsServices');
+const NewsService = require("../services/NewsServices");
 
 module.exports = {
-    searchAll: async (req, res)=>{
-        let json = {error:'', result:[]};
-        let news = await NewsService.searchAll();
-        for (let i in news){
-            json.result.push({
-                id: news[i].id,
-                author: news[i].author,
-                date: news[i].date,
-                title: news[i].title,
-                image: news[i].image
-            });
-        }
-        res.json(json);
-    },
-
-    searchAllReverse: async (req, res)=>{
-        let json = {error:'', result:[]};
-        let news = await NewsService.searchAllReverse();
-        for (let i in news){
-            json.result.push({
-                id: news[i].id,
-                author: news[i].author,
-                date: news[i].date,
-                title: news[i].title,
-                image: news[i].image
-            });
-        }
-        res.json(json);
-    },
-
-    searchNew: async (req, res)=>{
-        let json = {error:'', result:{}};
-
-        let id = req.params.id;
-        let New = await NewsService.searchNew(id);
-
-        if(New){
-            json.result = New;
-        }
-        res.json(json);
-    },
-
-    insertNew: async (req, res)=>{
-        let json = {error:'', result:{}};
-
-        let author = req.body.author;
-        let date = req.body.date;
-        let title = req.body.title;
-        let image = req.body.image;
-
-        if(author && date && title && image){
-            let NewCode = await NewsService.insertNew(author, date, title, image);
-            json.result = {
-                code: NewCode,
-                author,
-                date,
-                title,
-                image
-            };
-        }else {
-            json.error = 'Fields not sent';
-        }
-        res.json(json);
+  searchAll: async (req, res) => {
+    let json = { error: "", result: [] };
+    let news = await NewsService.searchAll();
+    for (let i in news) {
+      json.result.push({
+        id: news[i].id,
+        author: news[i].author,
+        date: news[i].date,
+        title: news[i].title,
+        image: news[i].image,
+      });
     }
+    res.json(json);
+  },
 
-}
+  searchAllReverse: async (req, res) => {
+    let json = { error: "", result: [] };
+    let news = await NewsService.searchAllReverse();
+    for (let i in news) {
+      json.result.push({
+        id: news[i].id,
+        author: news[i].author,
+        date: news[i].date,
+        title: news[i].title,
+        image: news[i].image,
+      });
+    }
+    res.json(json);
+  },
+
+  searchNew: async (req, res) => {
+    let json = { error: "", result: {} };
+
+    let id = req.params.id;
+    let New = await NewsService.searchNew(id);
+
+    if (New) {
+      json.result = New;
+    }
+    res.json(json);
+  },
+
+  insertNew: async (req, res) => {
+    let json = { error: "", result: {} };
+
+    let author = req.body.author;
+    let date = req.body.date;
+    let title = req.body.title;
+    let image = req.file ? req.file.originalname : null;
+
+    if (author && date && title && image) {
+      try {
+        let NewCode = await NewsService.insertNew(author, date, title, image);
+        json.result = {
+          code: NewCode,
+          author,
+          date,
+          title,
+          image,
+        };
+      } catch (error) {
+        json.error = "Error inserting new";
+      }
+    } else {
+      json.error = "Fields not sent or file not uploaded";
+    }
+    res.json(json);
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
-const variables = require('./config/variables.config');
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
-const routes = require('./routes');
+const variables = require("./config/variables.config");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const routes = require("./routes");
 
 const server = express();
 server.use(cors());
-server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
-server.use('/api',routes);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "assets/news");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+
+server.use(upload.single("image"));
+
+server.use("/api", routes);
 
 server.listen(variables.var.PORT, () => {
-    console.log(`Server running on: http://localhost:${variables.var.PORT}`);
+  console.log(`Server running on: http://localhost:${variables.var.PORT}`);
 });
